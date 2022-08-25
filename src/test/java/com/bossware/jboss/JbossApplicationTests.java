b2a5f750128c8d019970f55f396b25e70a9800b9
Ahmet Tarık Bostan <a.tarikbostan@gmail.com>
2022-08-26 00:00:44 +0300

    initialized base file structure

---
+++ src/test/java/com/bossware/jboss/JbossApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bossware.jboss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbossApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
